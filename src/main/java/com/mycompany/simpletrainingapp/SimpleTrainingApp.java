979546ba01412372f778b4a48cf54cecf8ba0421
Char893 <paparis060@gmail.com>
2023-10-12 11:31:21 -0300

    Finished startingPanel and started RoutinesPanel

--- src/main/java/com/mycompany/simpletrainingapp/SimpleTrainingApp.java
+++ src/main/java/com/mycompany/simpletrainingapp/SimpleTrainingApp.java
@@ -1,30 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.simpletrainingapp;
-
-import com.mycompany.simpletrainingapp.entities.Muscle;
-import com.mycompany.simpletrainingapp.entities.MuscleGroup;
-import com.mycompany.simpletrainingapp.repositories.HibernateUtil;
-import com.mycompany.simpletrainingapp.repositories.MuscleGroupRepository;
-import com.mycompany.simpletrainingapp.repositories.MuscleRepository;
 
 /**
  *
  * @author papar
  */
 public class SimpleTrainingApp {
+
     public static void main(String[] args) {
-        HibernateUtil.startActivity();
-        var muscle = new Muscle("Pepe");
-        var muscleGroup = new MuscleGroup("PepeGroup");
-        muscleGroup.addMuscle(muscle);
-        var mgr = new MuscleGroupRepository();
-        mgr.saveMuscleGroup(muscleGroup);
-        muscleGroup.removeMuscle(muscle);
-        var mr = new MuscleRepository();
-        mr.deleteMuscle(muscle);
-        HibernateUtil.endActivity();
+        java.awt.EventQueue.invokeLater(() -> new MainWindow().setVisible(true));
     }
 }
